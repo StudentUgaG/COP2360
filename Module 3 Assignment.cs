10221ed3694e5687a63f1d27aac4e0001f15ff0e
StudentUgaG <gavarsu@my.palmbeachstate.edu>
2025-02-16 14:14:22 -0500

    Update and rename Module 3 Assignment to Module 3 Assignment.cs

---
+++ Module 3 Assignment.cs
@@ -0,0 +1,18 @@
+Panda p = new Panda ("Sam");    //Constructor
+
+public class Panda
+{
+    string name;
+    public Panda (string n)
+    {
+        name = n;
+    }
+
+struct Spot     //Struct
+    {
+  public int white, black;
+  public Spot ()             { this.white = 6; this.black = 7; }
+  public Spot (int white, int black) { this.white = white; this.black = black; }
+    }
+
+}
